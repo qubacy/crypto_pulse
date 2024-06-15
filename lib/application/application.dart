b3ff9779777cec96ba6e8ce510d9658f54f2db5b
MCDead <souldzukaev@gmail.com>
2024-06-16 00:49:57 +0700

    Init commit;

---
+++ lib/application/application.dart
@@ -0,0 +1,18 @@
+import 'package:crypto_pulse/application/ui/screen/home.dart';
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crypto Pulse',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0x006974)),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'application/application.dart';
+
+void main() {
+  runApp(Application());
+}
