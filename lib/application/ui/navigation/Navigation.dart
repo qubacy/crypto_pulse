d8bbf8bb3c66c85ddf5511f81947eeb3dab5a79b
qubacy <souldzukaev@gmail.com>
2024-07-03 02:54:06 -0700

    Playing around with Navigation;

--- lib/application/ui/navigation/Navigation.dart
+++ lib/application/ui/navigation/Navigation.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_pulse/application/application.dart';
 import 'package:crypto_pulse/application/ui/screen/cryptocurrency/Cryptocurrencies.dart';
 import 'package:crypto_pulse/application/ui/screen/home/home.dart';
 import 'package:flutter/material.dart';
@@ -5,20 +6,32 @@ import 'package:flutter_svg_icons/flutter_svg_icons.dart';
 import 'package:go_router/go_router.dart';
 
 CustomTransitionPage getRoutePageBuilder(
+  String pagePath,
   GoRouterState state,
   Widget child 
 ) {
   return CustomTransitionPage(
     key: state.pageKey,
     child: child,
+    transitionDuration: Duration(seconds: 1),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      final currentName = navigatorContext != null ? ModalRoute.of(navigatorContext!)?.settings.name ?? pagePath : pagePath;
+      
+      print('getRoutePageBuilder(): pagePath = $pagePath; currentName = $currentName;');
+      
+      if (pagePath == currentName)
+        animation.addListener(() => print('pageKey: $pagePath; animation.value = ${animation.value};'));
+      else
+        secondaryAnimation.addListener(() => print('pageKey: $pagePath; secondaryAnimation.value = ${secondaryAnimation.value};'));
+
       return SlideTransition(
         position:
           Tween(
             begin: const Offset(1, 0),
             end: Offset.zero
-          ).animate(animation),
-        child: child);
+          ).animate(pagePath == currentName ? animation : secondaryAnimation),
+        child: child
+      );
     },
   );
 }
@@ -28,14 +41,14 @@ final routes = [
     path: Home.PATH,
     name: Home.NAME,
     pageBuilder: (context, state) {
-      return getRoutePageBuilder(state, Home());
+      return getRoutePageBuilder(Home.PATH, state, Home());
     },
   ),
   GoRoute(
     path: Cryptocurrencies.PATH,
     name: Cryptocurrencies.NAME,
     pageBuilder: (context, state) {
-      return getRoutePageBuilder(state, const Cryptocurrencies());
+      return getRoutePageBuilder(Cryptocurrencies.PATH, state, const Cryptocurrencies());
     }
   )
 ];
